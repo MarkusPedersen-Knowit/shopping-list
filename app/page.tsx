f2adb73d9a3acda517b70a3ea88e146cf60f3fbd
Markus Pedersen <markus.pedersen@knowit.no>
2025-01-17 12:26:03 +0100

    fixed instant removal of toggled items

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         }
       });
     }
-  }, [loading, items, deleteItem]);
+  }, [loading]);
 
   if (loading) {
     return <div>Laster handleliste...</div>;
